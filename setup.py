2462a1360c28fad21e8fbb13ad2c4d78728140ec
Atharv787 <75237864+Atharv787@users.noreply.github.com>
2024-07-31 14:31:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-    name                =   "rapidscan",
+    name                =   "webscan",
     version             =   '1.2',
     description         =   "The Multi-Tool Web Vulnerability Scanner.",
     long_description    =   README,
